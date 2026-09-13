396fd00182689503eef81c4a252d253fc996f92a
xaliabdullah176-exe <x.ali.abdullah176@gmail.com>
2026-09-13 17:08:08 +0500

    Fixing Linear Search Bug in ArrayList

--- SemesterNo03/DataStructures_C++/DS_PracticeCpp/ArrayList.cpp
+++ SemesterNo03/DataStructures_C++/DS_PracticeCpp/ArrayList.cpp
@@ -1,140 +1,275 @@
+// #include <iostream>
+// #include <cstring>
+// #include <string>
+// #include <cmath>
+// using namespace std;
+
+// class ArrayList // Class Declaration
+// {
+
+//     int size;
+//     int totalCapacity;
+//     int *arr;
+
+// public:
+//     ArrayList() // Array List Constructors
+//     {
+//         size = 0;
+//         totalCapacity = 5;
+//         arr = new int[totalCapacity];
+//     }
+//     ArrayList(int size, int totalCapacity)
+//     {
+//         this->size = size;
+//         this->totalCapacity = totalCapacity;
+//     }
+//     void insert(int value) // insert function
+//     {
+//         if (isFull())
+//         {
+//             cout << "Array is Growing" << endl;
+//             reGrow();
+//         }
+//         arr[size] = value;
+//         size++;
+//     }
+
+//     bool isFull() // Checker
+//     {
+//         if (size == totalCapacity)
+//             return true;
+//         return false;
+//     }
+//     void reGrow() // ReGrow Function
+//     {
+//         int *temp = new int[2 * size];
+
+//         for (int i = 0; i < size; i++)
+//         {
+//             temp[i] = arr[i];
+//         }
+
+//         delete[] arr;
+//         arr = temp;
+//         totalCapacity *= 2;
+//     }
+//     void display() // Display Function
+//     {
+//         for (int i = 0; i < size; i++)
+//         {
+//             cout << *(arr + i) << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     int LinearSearch(int targetValue)
+//     {
+//         for (int i = 0; i < size; i++)
+//         {
+//             if (arr[i] == targetValue)
+//                 return i;
+//         }
+//         return -1;
+//     }
+//     void removeData(int data)
+//     {
+//         if (isEmpty())
+//             cout << "ArrayList is Full.. No Data to delete" << endl;
+
+//         else
+//         {
+//             int index = LinearSearch(data);
+
+//             int i;
+//             while(i < size)
+//             {
+//                 if (arr[i] != index)
+//                 cout << "there is no such index exist against which the data should delete"<<endl;
+//                 i++;
+//             }
+
+//             if (data == size)
+//             {
+//                 size--;
+//             }
+//             else
+//             {
+//                 for(int i = index; i<size-1; i++)
+//                 {
+//                    arr[i] = arr[i+1];
+//                 }
+//                 size--;
+//             }
+//         }
+//     }
+//     bool isEmpty()
+//     {
+//         if (size == 0)
+//             return true;
+//         return false;
+//     }
+// };
+
+// int main() // Main Function
+// {
+//     /*Creating methods in the ArrayList()
+//     1-Insert 2-Search 3-Display 4-ReGrow 5-Delete 6-Update...*/
+//     ArrayList alist;
+//     alist.insert(10);
+//     alist.insert(20);
+//     alist.insert(30);
+//     alist.insert(40);
+//     alist.insert(50);
+//     alist.insert(90);
+//     alist.display();
+
+//     cout << endl;
+
+//     int index = alist.LinearSearch(90);
+
+//     if (index == -1)
+//     {
+//         cout << "Data Not Found in ArrayList()" << endl;
+//     }
+//     else
+//     {
+//         cout << "The Data is found at the index " << index << endl;
+//     }
+//     alist.removeData(20);
+//     return 0;
+// }
+
 #include <iostream>
+#include <stdexcept>
 #include <cstring>
 #include <string>
 #include <cmath>
+
 using namespace std;
 
-class ArrayList // Class Declaration
+class ArrayList
 {
-
+private:
     int size;
-    int totalCapacity;
+    int TotalCapacity;
     int *arr;
 
 public:
-    ArrayList() // Array List Constructors
+    // Default and Overloaded Constructors
+    ArrayList()
     {
         size = 0;
-        totalCapacity = 5;
-        arr = new int[totalCapacity];
+        TotalCapacity = 5;
+        arr = new int[TotalCapacity];
     }
-    ArrayList(int size, int totalCapacity)
+
+    ArrayList(int size, int TotalCapacity)
     {
         this->size = size;
-        this->totalCapacity = totalCapacity;
+        this->TotalCapacity = TotalCapacity;
+        // this->arr = new int[TotalCapacity];
     }
-    void insert(int value) // insert function
+    // Insert Function
+    void insert(int value)
     {
         if (isFull())
         {
-            cout << "Array is Growing" << endl;
+            cout << "The Array is Growing " << endl;
             reGrow();
         }
         arr[size] = value;
         size++;
     }
-
-    bool isFull() // Checker
+    // Checker Bool
+    bool isFull()
     {
-        if (size == totalCapacity)
+        if (size == TotalCapacity)
             return true;
         return false;
     }
-    void reGrow() // ReGrow Function
+    // Regrow Function
+    void reGrow()
     {
-        int *temp = new int[2 * size];
-
+        int *temp = new int[2 * TotalCapacity];
         for (int i = 0; i < size; i++)
         {
             temp[i] = arr[i];
         }
-
         delete[] arr;
         arr = temp;
-        totalCapacity *= 2;
+        TotalCapacity *= 2;
     }
-    void display() // Display Function
+    // Display Function
+    void display()
     {
         for (int i = 0; i < size; i++)
         {
             cout << *(arr + i) << " ";
         }
-        cout << endl;
     }
-
-    int LinearSearch(int targetValue)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            if (arr[i] == targetValue)
-                return i;
-        }
-        return -1;
-    }
-    void removeData(int data)
-    {
-        if (isEmpty())
-            cout << "ArrayList is Full.. No Data to delete" << endl;
-
-        else
-        {
-            int index = LinearSearch(data);
-
-            int i;
-            while(i < size)
-            {
-                if (arr[i] != index)
-                cout << "there is no such index exist against which the data should delete"<<endl;
-                i++;
-            }
-          
-            if (data == size)
-            {
-                size--;
-            }
-            else
-            {
-                for(int i = index; i<size-1; i++)
-                {
-                   arr[i] = arr[i+1];
-                }
-                size--;
-            }
-        }
-    }
+    //Checks if the Array is Empty;
     bool isEmpty()
     {
-        if (size == 0)
-            return true;
-        return false;
+     if(size == 0)
+         return true;
+    return false;
+    }
+    // LinearSearch Algorithm
+    int linearSearch(int targetValue)
+    {
+         if(isEmpty())
+         {
+            cout<<"The Array is Empty Please Recheck and Add Elements "<<endl;
+            return -2;
+         }
+         else
+         {
+            for(int i = 0; i<size; i++)
+            {
+                if (arr[i] = targetValue)
+                    return i;    
+            }
+            return -1;
+         }
+    }
+
+    void clearArray()
+    {
+        size = 0;
+        TotalCapacity = 5;
+        arr = new int[TotalCapacity];
+    }
+    
+    void removeElements(int element)
+    {
+        if(!isEmpty())
+        {
+            int index = linearSearch(element);
+
+        }
     }
 };
 
-int main() // Main Function
+int main()
 {
-    /*Creating methods in the ArrayList()
-    1-Insert 2-Search 3-Display 4-ReGrow 5-Delete 6-Update...*/
-    ArrayList alist;
-    alist.insert(10);
-    alist.insert(20);
-    alist.insert(30);
-    alist.insert(40);
-    alist.insert(50);
-    alist.insert(90);
-    alist.display();
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    cout << endl;
+    ArrayList Array;
+    Array.insert(10);
+    Array.insert(20);
+    Array.insert(30);
+    Array.insert(40);
+    Array.insert(50);
+    Array.insert(50);
+        
+    Array.display();
 
-    int index = alist.LinearSearch(90);
+    cout<<endl;
+   
+    int index = Array.linearSearch(30);
+    
+    cout << index;
 
-    if (index == -1)
-    {
-        cout << "Data Not Found in ArrayList()" << endl;
-    }
-    else
-    {
-        cout << "The Data is found at the index " << index << endl;
-    }
-    alist.removeData(20);
     return 0;
 }
