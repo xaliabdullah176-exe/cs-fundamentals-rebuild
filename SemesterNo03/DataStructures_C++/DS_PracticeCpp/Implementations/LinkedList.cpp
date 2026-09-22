1441b12782988e898559b3cad57d3be75b0b1432
xaliabdullah176-exe <x.ali.abdullah176@gmail.com>
2026-09-22 23:54:48 +0500

    Intializing the Data Structures...

---
+++ SemesterNo03/DataStructures_C++/DS_PracticeCpp/Implementations/LinkedList.cpp
@@ -0,0 +1,158 @@
+// #include <iostream>
+// using namespace std;
+
+// class SinglyLinkedList
+// {
+
+//     class Node
+//     {
+//     public:
+//         int data;
+//         Node *next;
+//         Node()
+//         {
+//             data = 0;
+//             next = NULL;
+//         }
+
+//         Node(int data)
+//         {
+//             this->data = data;
+//             next = NULL;
+//         }
+//     };
+
+// private:
+//     Node *head;
+//     Node *tail;
+
+// public:
+//     SinglyLinkedList()
+//     {
+//         head = tail = NULL;
+//     }
+
+//     void push_front(int data) // O(1)
+//     {
+//         Node *newNode = new Node(data);
+//         if (head == NULL)
+//         {
+//             head = tail = newNode;
+//         }
+//         else
+//         {
+//             newNode->next = head;
+//             head = newNode;
+//         }
+//     }
+
+//     void push_back(int data) // O(n)
+//     {
+//         Node *newNode = new Node(data);
+
+//         if (head == NULL)
+//         {
+//             head = tail = newNode;
+//         }
+//         else
+//         {
+//             Node *temp = head;
+
+//             while (temp->next != NULL)
+//             {
+//                 temp = temp->next;
+//             }
+//             temp->next = newNode;
+//         }
+//     }
+//     void pop_front(int target)
+//     {
+//         if (head == NULL)
+//         {
+//             cout << "List is Empty " << endl;
+//             return;
+//         }
+//         Node *temp = head;
+//         head = head->next;
+//         temp->next = NULL;
+
+//         delete temp;
+//     }
+
+//     void pop_back()
+//     {
+//         Node *temp = head;
+//         if(head == NULL)
+//         {
+//             cout<<"Empty Nothing to delete"<<endl;
+//         }
+//         else
+//         {
+//         while(temp->next->next!=NULL)
+//         {
+//             temp = temp -> next;
+           
+            
+//         }
+//          delete temp;
+
+//         }
+//     }
+
+//     void print_list()
+//     {
+//         if (head == NULL)
+//         {
+//             cout << "List is Empty" << endl;
+//         }
+//         else
+//         {
+//             Node *temp = head;
+
+//             while (temp != NULL)
+//             {
+//                 cout << temp->data << " -> ";
+//                 temp = temp->next;
+//             }
+//         }
+//         cout << " NULL" << endl;
+//     }
+// };
+// int main()
+// {
+
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     SinglyLinkedList li;
+//     li.push_front(20);
+//     li.push_front(30);
+//     li.print_list();
+
+//     return 0;
+// }
+
+
+
+
+// Linked List New Implementation: 
+
+#include <iostream>
+using namespace std;
+
+class LinkedList{
+    
+    struct Node{
+
+    };
+
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
--- SemesterNo03/DataStructures_C++/StandardTemplateLibrary/STL1_Vectors.cpp
+++ SemesterNo03/DataStructures_C++/StandardTemplateLibrary/STL1_Vectors.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
     
+    vector<int> vec;
+    
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    vec.push_back(40);
+    vec.push_back(60);
+    vec.push_back(90);
+
+    for(int val : vec)
+    {
+         cout<< vec.
+    }
+
+    cout<<endl;
+    
+    vector<int> vec2(5,-999);
+     
+    cout<<endl;
+
+    vector<int> vec3 = {1,2,3,4,5};
+
+    cout<<endl;
+
+    vector<int>vec1(vec);
+
 
     return 0;
 }
